d2899de2774aae529504882ba60beed0cafcf2a0
Roy <codefighter@foxmail.com>
2017-09-29 12:58:15 +0800

    完成了闹钟基本逻辑，下一步完善界面

--- TomatoAlarm/CAlarm.cs
+++ TomatoAlarm/CAlarm.cs
@@ -1,13 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Threading;
 
 namespace TomatoAlarm
 {
-    public class CAlarm
+    public delegate void TimeOutEventHandler(object sender, EventArgs e);
+    public class CAlarm:INotifyPropertyChanged
     {
+
+        private DispatcherTimer timer;
+
+        private TimeSpan timeleft;
+        private TimeSpan timelimit;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event TimeOutEventHandler TimeOut;
+
+        public CAlarm()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,1);
+            timer.Tick += RefreshTimeLeft;
+        }
+
+        private void RefreshTimeLeft(object sender, EventArgs e)
+        {
+            TimeLeft = TimeLeft.Subtract(new TimeSpan(0,0,1));
+        }
+
+        public TimeSpan TimeLimit
+        {   get
+            {
+                return timelimit;
+            }
+            set
+            {
+                timelimit = value;
+                TimeLeft  = value;
+            }
+        }
         
+        public TimeSpan TimeLeft
+        {
+            get
+            {
+                return timeleft;
+            }
+            private set
+            {
+                timeleft = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TimeLeft"));
+                
+                if(timeleft.TotalSeconds <= 0)
+                {
+                    timer.Stop();
+                    TimeOut?.Invoke(this,new EventArgs());
+                }
+            }
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Pause()
+        {
+            timer.Stop();
+        }
     }
 }
--- TomatoAlarm/MainWindow.xaml.cs
+++ TomatoAlarm/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using System.Timers;
 namespace TomatoAlarm
 {
     /// <summary>
@@ -20,6 +8,8 @@ namespace TomatoAlarm
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CAlarm alarm;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,6 +23,18 @@ namespace TomatoAlarm
             {
                 MessageBox.Show("No setting stored.");
             }
+
+            alarm = new CAlarm {TimeLimit = new TimeSpan(0,1,0)};
+
+            this.DataContext = alarm;
+            alarm.Start();
+
+            alarm.TimeOut += Alarm_TimeOut;
+        }
+
+        private void Alarm_TimeOut(object sender, EventArgs e)
+        {
+            this.Background = System.Windows.Media.Brushes.Red;
         }
 
         private void onclosing(object sender, System.ComponentModel.CancelEventArgs e)
